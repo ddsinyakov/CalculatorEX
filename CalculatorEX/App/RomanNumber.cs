3689522607b05c0ebfe6015da4d082730382826b
ddsinyakov <dimasiniyakovwork@gmail.com>
2022-09-02 11:51:09 +0300

    RomanNumber.Parse() completed. All tests completed successfuly

---
+++ CalculatorEX/App/RomanNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorEX.App
+{
+    public class RomanNumber
+    {
+        static public int Parse(String str)
+        {
+            char[] digits = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+            int[] digitsValues = { 1, 5, 10, 50, 100, 500, 1000 };
+
+            char digit = str[str.Length - 1];
+            int index = Array.IndexOf(digits, digit);
+
+            if(index == -1)
+            {
+                throw new ArgumentException($"Invalid char {digit}");
+            }
+
+            int val = digitsValues[index];
+            int res = val;
+
+            for (int i = str.Length - 2; i>= 0; i--)
+            {
+                digit = str[i];
+                index = Array.IndexOf(digits, digit);
+
+                if (index == -1)
+                {
+                    throw new ArgumentException($"Invalid char {digit}");
+                }
+
+                if(digitsValues[index] < val)
+                {
+                    res -= digitsValues[index];
+                }
+                else
+                {
+                    val = digitsValues[index];
+                    res += val;
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ CalculatorTests/CalcTests.cs
@@ -0,0 +1,31 @@
+using CalculatorEX.App;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void TestCalc()
+        {
+            var calc = new Calc();
+
+            Assert.IsNotNull(calc);
+        }
+
+        [TestMethod]
+        public void RomanNumberParse()
+        {
+            Assert.AreEqual(RomanNumber.Parse("I"), 1, "I == 1");
+            Assert.AreEqual(RomanNumber.Parse("IV"), 4, "IV == 4");
+            Assert.AreEqual(RomanNumber.Parse("XV"), 15);
+            Assert.AreEqual(RomanNumber.Parse("XXX"), 30);
+            Assert.AreEqual(RomanNumber.Parse("CM"), 900);
+            Assert.AreEqual(RomanNumber.Parse("MCMXCIX"), 1999);
+            Assert.AreEqual(RomanNumber.Parse("CD"), 400);
+            Assert.AreEqual(RomanNumber.Parse("CDI"), 401);
+            Assert.AreEqual(RomanNumber.Parse("LV"), 55);
+            Assert.AreEqual(RomanNumber.Parse("XL"), 40);
+        }
+    }
+}
