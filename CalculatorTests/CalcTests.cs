052986e02c1ddda8157ba41a399c0814f2c1ac0e
ddsinyakov <dimasiniyakovwork@gmail.com>
2022-09-07 13:13:49 +0300

    Comments added for tests

--- CalculatorTests/CalcTests.cs
+++ CalculatorTests/CalcTests.cs
@@ -16,6 +16,7 @@ namespace CalculatorTests
         [TestMethod]
         public void RomanNumberParse1Digits()
         {
+            // Check if RomanNumber.Parse works with 1 simbols
             Assert.AreEqual(RomanNumber.Parse("I"), 1);
             Assert.AreEqual(RomanNumber.Parse("V"), 5);
             Assert.AreEqual(RomanNumber.Parse("X"), 10);
@@ -29,6 +30,7 @@ namespace CalculatorTests
         [TestMethod]
         public void RomanNumberParse2Digits()
         {
+            // Check if RomanNumber.Parse works with 2 simbols
             Assert.AreEqual(RomanNumber.Parse("IV"), 4);
             Assert.AreEqual(RomanNumber.Parse("XV"), 15);
             Assert.AreEqual(RomanNumber.Parse("CM"), 900);
@@ -41,6 +43,7 @@ namespace CalculatorTests
         [TestMethod]
         public void RomanNumberParse3MoreDigits()
         {
+            // Check if RomanNumber.Parse works with 3+ simbols 
             Assert.AreEqual(RomanNumber.Parse("XXX"), 30);
             Assert.AreEqual(RomanNumber.Parse("CDI"), 401);
             Assert.AreEqual(RomanNumber.Parse("MCMXCIX"), 1999);
@@ -49,17 +52,22 @@ namespace CalculatorTests
         [TestMethod]
         public void RomanNumberParseInvalid()
         {
+            // Check if invalid simbol at first place in argument of RomanNumber.Parse throws ArgumentException
             var exc = Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse("AXX"));
-            var exp = new ArgumentException("Invalid char A");
 
+            // Check if invalid simbol at first place in argument of RomanNumber.Parse throws exception with "Invalid char A" message
+            var exp = new ArgumentException("Invalid char A");
             Assert.AreEqual(exp.Message, exp.Message);
 
+            // Same for invalid simbol in the middle
             exc = Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse("XAX"));
             Assert.AreEqual(exp.Message, exp.Message);
 
+            // Same for invalid simbol at last place
             exc = Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse("XXA"));
             Assert.AreEqual(exp.Message, exp.Message);
 
+            // Check if exception message starts with "Invalid char"
             exc = Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse("X X"));
             Assert.IsTrue(exp.Message.StartsWith("Invalid char"));
         }
@@ -67,11 +75,14 @@ namespace CalculatorTests
         [TestMethod]
         public void RomanNumberParseEmpty()
         {
+            // Check if RomanNumber.Parse with empty string argument throws an ArgumentException 
             var exc = Assert.ThrowsException<ArgumentException>(() => RomanNumber.Parse(""));
-            var exp = new ArgumentException("Empty string not allowed");
 
+            // Check if RomanNumber.Parse with empty string argument exception message is equal "Empty string not allowed"
+            var exp = new ArgumentException("Empty string not allowed");
             Assert.AreEqual(exc.Message, exp.Message);
 
+            // Check if RomanNumber.Parse with null argument throws ArgumentNullException
             Assert.ThrowsException<ArgumentNullException>(() => RomanNumber.Parse(null!));
         }
     }
