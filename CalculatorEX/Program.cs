3f7fb7f7eaccec7b4dcfc861f184d69e40fcf3a6
ddsinyakov <dimasiniyakovwork@gmail.com>
2022-09-07 11:51:45 +0300

    Remove all the comments added before. Add tests for RomanNumber.Parse

--- CalculatorEX/Program.cs
+++ CalculatorEX/Program.cs
@@ -1,7 +1,2 @@
-﻿Console.WriteLine("Calculator is running");
+﻿
 new CalculatorEX.App.Calc().Run();
-// Hello From Vs
-new CalculatorEX.App.Calc().Run();
-//Hello from Github
-//hello world :)
-// new comment
